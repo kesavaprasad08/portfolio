3fd747eafa7f578357a16dd4744454cd282e0c7a
kesavaprasad08 <kesav2661998@gmail.com>
2024-03-20 00:05:09 +0530

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,33 @@
 const Home = () => {
   return (
     <section
-      className=" flex flex-col lg:flex-row items-center justify-center bg-pageGreen h-screen p-10 text-white pt-14 "
+      className=" flex flex-col lg:flex-row   items-center bg-pageGreen  p-10 text-white pt-14 lg:h-screen "
       id="Home"
     >
-      <div className="flex flex-col ">
-        <h1 className="text-5xl p-2">Hi, I'm KESAVA PRASAD</h1>
+      <div className="flex flex-col lg:ml-10 mt-18 flex-wrap p-1 lg:pr-20 ">
+      <img
+        src="https://media.licdn.com/dms/image/D5603AQFJUUPNXYL5ew/profile-displayphoto-shrink_800_800/0/1701245934705?e=1716422400&v=beta&t=RAN8Zf-2CN8arq53nq1TUiUNsnwh3MKoUdNKWJypXbw"
+        alt="kesav"
+        className="w-64 rounded-full mb-6 lg:hidden"
+      />
+        <h1 className="text-5xl p-2 ">Hi, I'm KESAVA PRASAD</h1>
         <p className="text-2xl p-2 text-green-500"> Web Developer</p>
-        <p className="w-6/12 p-2">
-      Full Stack Web Developer adept at building responsive web applications. Passionate about Learning new technologies,bringing ideas to life and working with dedicated teams to build efficient and robust applications suited to the user's needs.
+        <p className=" pt- pb-5 p-2  ">
+        Full Stack Web Developer adept at building responsive web applications. Passionate about Learning new technologies,bringing ideas to life and working with dedicated teams to build efficient and robust applications suited to the user's needs.
       </p>
-        <a href="#portfolio" className="p-2">
-          <button className="bg-green-500 p-2 ">View My Works</button>
+      <div>
+        <a href="#portfolio" className="lg:p-2">
+          <button className="bg-green-500 p-2 hover:bg-green-300 ">View My Works</button>
         </a>
+        <a href="#portfolio" className="lg:p-2 pl-2">
+          <button className="bg-green-500 p-2 hover:bg-green-300">My Resume</button>
+        </a>
+        </div>
       </div>
       <img
         src="https://media.licdn.com/dms/image/D5603AQFJUUPNXYL5ew/profile-displayphoto-shrink_800_800/0/1701245934705?e=1716422400&v=beta&t=RAN8Zf-2CN8arq53nq1TUiUNsnwh3MKoUdNKWJypXbw"
         alt="kesav"
-        className="w-64 rounded-full"
+        className="w-64 rounded-full hidden lg:block"
       />
 
      
